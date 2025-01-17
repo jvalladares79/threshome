3947e851439de2f5a9b7c58cd59d0efcc4884e77
Jasmine Valladares <jvalladares@icstars.org>
2025-01-16 18:28:02 -0600

    my-vite-app

---
+++ my-vite-app/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ProfilePic from './ProfilePic'
+
+export default function App() {
+  return (
+    <div>
+        <ProfilePic/>
+    </div>
+  )
+}
---
+++ my-vite-app/src/ProfilePic.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './assets/logo 1.png';
+
+export default function ProfilePic() {
+  return (
+    <div>
+<img src={logo} alt="ThresHomeLogo" width="270" height="150"/>
+    <input type="text" placeholder="search"className='Search'/> 
+    <div>
+      <p class="ProfilePic"></p>
+    
+    </div>
+    <h1></h1>
+
+    </div>
+  )
+}
+
